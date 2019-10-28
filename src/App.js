1984f39d43635453e126f3782dad1b1479f19825
ryan <1rmills1.rm@gmail.com>
2019-10-28 09:13:42 -0500

    final

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import Header from './components/layout/Header'
+import Todos from './components/Todos'
+import AddTodo from './components/AddTodo'
+import uuid from 'uuid';
+
 import './App.css';
 
-function App() {
+
+
+
+class App extends Component {
+state = {
+  todos: [
+    {
+      id: uuid.v4(),
+      title: 'Take out the trash',
+      completed: false
+    },{
+      id: uuid.v4(),
+      title: 'Make dinner',
+      completed: false
+    },{
+      id: uuid.v4(),
+      title: 'Buy groceries',
+      completed: false
+    }
+  ]
+}
+
+markComplete = (id)=>{
+  this.setState({todos: this.state.todos.map(todo => {
+    if(todo.id === id){
+      todo.completed = !todo.completed
+    }
+    return todo;
+  })})
+}
+delTodo = (id) => {
+  this.setState({todos: [...this.state.todos.filter(todo =>todo.id !== id)]})
+}
+addTodo = (title) => {
+  const newTodo = {
+    id:  uuid.v4(),
+    title: title,
+    completed: false
+  }
+  this.setState({
+    todos: [...this.state.todos, newTodo]
+  })
+}
+
+
+  render(){
+    console.log(this.state.todos)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+      <Header />
+      <AddTodo addTodo={this.addTodo}/>
+      <Todos todos={this.state.todos} 
+      markComplete={this.markComplete}
+      delTodo={this.delTodo} 
+      />
+      </div>
+     
     </div>
   );
+}
 }
 
 export default App;
